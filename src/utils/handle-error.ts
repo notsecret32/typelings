e38ddd99f015d425d3f4d29bbeb16ff55322ea21
Alexander Skvorchevskiy <43242733+notsecret32@users.noreply.github.com>
2024-12-09 18:00:28 +0300

    feat: added validation of `zod` errors

--- src/utils/handle-error.ts
+++ src/utils/handle-error.ts
@@ -1,8 +1,19 @@
+import { highlighter } from '@/utils/highlighter';
 import { logger } from '@/utils/logger';
+import { z } from 'zod';
 
 export function handleError(error: unknown) {
   if (typeof error === 'string') {
     logger.error(error);
+    logger.break();
+    process.exit(1);
+  }
+
+  if (error instanceof z.ZodError) {
+    logger.error('Validation failed:');
+    for (const [key, value] of Object.entries(error.flatten().fieldErrors)) {
+      logger.error(`- ${highlighter.info(key)}: ${value}`);
+    }
     logger.break();
     process.exit(1);
   }
