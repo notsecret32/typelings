f6824ca70a3758560edd2ca52c352e7274a40c25
Alexander Skvorchevskiy <43242733+notsecret32@users.noreply.github.com>
2024-12-09 17:18:08 +0300

    feat: added a description of the 'init` command

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -2,11 +2,14 @@ import { handleError } from '@/utils/handle-error';
 import { logger } from '@/utils/logger';
 import { Command } from 'commander';
 
-export const init = new Command().name('init').action(async () => {
-  try {
-    logger.log('Init command');
-  } catch (error) {
-    logger.break();
-    handleError(error);
-  }
-});
+export const init = new Command()
+  .name('init')
+  .description('initialize your project and install dependencies')
+  .action(async () => {
+    try {
+      logger.log('Init command');
+    } catch (error) {
+      logger.break();
+      handleError(error);
+    }
+  });
