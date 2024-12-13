e0e2e97e243a2b25126fa42d8b08a3d1d7461a28
Alexander Skvorchevskiy <alex.skvorchevsky@gmail.com>
2024-12-13 19:54:04 +0300

    chore: configure typescript & jest

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,11 @@ import type { Config } from 'jest';
 
 const config: Config = {
   preset: 'ts-jest/presets/default-esm',
+  roots: ['./src', './test'],
+  moduleDirectories: ['node_modules', 'src'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
 
   // Mocks
   clearMocks: true,
